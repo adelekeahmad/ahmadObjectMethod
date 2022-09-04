6d937e2fd7fe0c6d002ce8fda8556f1e8b5faaed
Ahmad Bakare <65125857+adelekeahmad@users.noreply.github.com>
2022-09-04 12:50:47 -0700

    Add project files.

---
+++ ahmadObjectMethod/ObjectMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ahmadObjectMethod
+{
+    internal class ObjectMethod
+    {
+        public string name; 
+        public int level; 
+        public double CGPA;
+        public ObjectMethod(string aName, int aLevel, double aCGPA)
+        {
+            name = aName;
+            level = aLevel;
+            CGPA = aCGPA;
+        }
+        public bool hasHonours()
+        {
+            if(CGPA >= 6.1)
+            {
+                return true;
+            }
+            return false;
+        }
+            }
+}
---
+++ ahmadObjectMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ahmadObjectMethod{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            ObjectMethod student1 = new ObjectMethod("Ahmad", 700, 6.0);
+            ObjectMethod student2 = new ObjectMethod("Timmy", 700, 6.5);
+            ObjectMethod student3 = new ObjectMethod("Shanoy", 700, 5.0);
+            Console.WriteLine(student3.hasHonours());
+            Console.ReadLine();
+        }
+    }
+}
